c8ce9fd47fec785b4e6d4a3ab5cfe8e9f38e898a
VenkateshSrinivasan <venkatesh-5.srinivasan-5@cognizant.com>
2020-02-29 19:36:50 +0530

    committed the functions adn started stored proc

--- Functions.sql
+++ Functions.sql
@@ -9,3 +9,22 @@ RETURN ages;
 END;
 $ages$ LANGUAGE plpgsql;  
 select MyAgecal('1978-04-03')
+--2
+
+CREATE SEQUENCE "Person_Id_seq"
+  INCREMENT 1
+  MINVALUE 1
+  MAXVALUE 9223372036854775807
+  START 1
+  CACHE 1;
+drop table person
+CREATE TABLE person
+(
+  P_Id integer NOT NULL DEFAULT nextval('public."Person_Id_seq"'::regclass),
+  pname varchar(30),
+  birthdate timestamp,
+  currentAge int
+)
+select * from person
+
+insert into person (pname,birthdate,currentAge) values ('venkat','1978-04-03',MyAgecal('1978-04-03'))
